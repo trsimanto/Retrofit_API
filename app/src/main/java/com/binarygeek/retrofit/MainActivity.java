ff15c9b36b59762fd06d1c0e23d76a6c9111a290
Towhid <towhidurrahmansimanto@gmail.com>
2019-01-03 17:28:21 +0600

    Signup and create user/user exist

---
+++ app/src/main/java/com/binarygeek/retrofit/MainActivity.java
@@ -0,0 +1,124 @@
+package com.binarygeek.retrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/*
+ * Sign Up Activity
+ * */
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextEmail, editTextPassword, editTextName, editTextSchool;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextName = findViewById(R.id.editTextName);
+        editTextSchool = findViewById(R.id.editTextSchool);
+
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+        findViewById(R.id.textViewLogin).setOnClickListener(this);
+    }
+
+    private void userSignUp() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String name = editTextName.getText().toString().trim();
+        String school = editTextSchool.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Password should be atleast 6 character long");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (name.isEmpty()) {
+            editTextName.setError("Name required");
+            editTextName.requestFocus();
+            return;
+        }
+
+        if (school.isEmpty()) {
+            editTextSchool.setError("School required");
+            editTextSchool.requestFocus();
+            return;
+        }
+
+        Call<ResponseBody> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .createUser(email, password, name, school);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    if (response.code() == 201) {
+                        String s = response.body().string();
+                        Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        String s = response.errorBody().string();
+                        Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonSignUp:
+                userSignUp();
+                break;
+            case R.id.textViewLogin:
+
+                break;
+        }
+    }
+}
